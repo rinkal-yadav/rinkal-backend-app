2cb1a7ceb33808fb77a9186c27c39ee0cb1555e4
rinkal-yadav <rinkal95@gmail.com>
2022-02-18 13:14:35 +0400

    blog Api

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 const express = require("express");
-// const cors =require("cors")
+const cors =require("cors")
 const router = require("./routes")
 const app=express()
-// app.use(cors())
+app.use(cors())
 
 app.use("/api/v1",router);
 
