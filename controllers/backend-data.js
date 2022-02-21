f38c8df6f3c93242507c5b9ace37b79fdbfcd6b5
rinkal-yadav <rinkal95@gmail.com>
2022-02-21 21:59:44 +0400

    commit

--- controllers/backend-data.js
+++ controllers/backend-data.js
@@ -1,5 +1,7 @@
 const data=(req,res)=>{
-    res.send([
+    console.log("*******Request query************", req.query);
+    console.log("*******Request query************", req.params);
+    const user=[
         {
             Id: "1",
             Category: "business",
@@ -364,7 +366,20 @@ const data=(req,res)=>{
         
 
 
-    ])
+    ]
+
+    if(req.query.Id){
+        const newuser =user.filter(value=>value.Id===req.query.Id)
+        res.send(newuser);
+    } else if(req.params.Category){
+        const newuser =user.filter(value=>value.Category===req.params.Category)
+        res.send(newuser);
+    }else{
+        res.send(user)
+    }
+    
+    
+
 }
 
 module.exports.blogData = data
